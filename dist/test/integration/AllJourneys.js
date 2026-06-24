b78d02af89f502e1f91723589b64085607aae11d
varun mohan <varunmohan766@gmail.com>
2026-06-24 14:10:15 +0000

    update index resource

---
+++ dist/test/integration/AllJourneys.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/test/Opa5","./arrangements/Startup","./NavigationJourney"],function(e,t){"use strict";e.extendConfig({arrangements:new t,viewNamespace:"com.bot.resto.restaurantbot.view.",autoWait:true})});
+//# sourceMappingURL=AllJourneys.js.map
