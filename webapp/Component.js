2e374b4206260e5a3a95a07dcdb36ea04d3e83c6
varun mohan <varunmohan766@gmail.com>
2026-08-09 18:41:51 +0000

    add miss api

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,7 +20,7 @@ sap.ui.define([
             this.setModel(models.createDeviceModel(), "device");
 
             
-            var oData = new sap.ui.model.json.JSONModel();
+            var oData = new sap.ui.model.json.JSONModel({api:"https://clientapi.ask-assistant.com/"});
             // oData.loadData("model/data.json");
 
             oData.loadData(sap.ui.require.toUrl("com/bot/resto/restaurantbot/model/data.json"));
